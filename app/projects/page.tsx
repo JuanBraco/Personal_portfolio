4b10536b9061a550dc9b99ba7d57c13aeec897df
Juan Braco <107987620+ght2121@users.noreply.github.com>
2023-06-14 17:12:40 +0200

    go

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -37,7 +37,7 @@ export default async function ProjectsPage() {
 						Projects
 					</h2>
 					<p className="mt-4 text-zinc-400">
-						Some of the projects are from scholl and some are on my own time.
+						Some of the projects are from school and some are on my own time.
 					</p>
 				</div>
 				<div className="w-full h-px bg-zinc-800" />
