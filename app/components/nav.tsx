465114b9fa99e0cfef35f97c8287e4ba978caec8
Juan Braco <107987620+ght2121@users.noreply.github.com>
2023-12-04 12:47:34 +0100

    little modif

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -40,6 +40,12 @@ export const Navigation: React.FC = () => {
 						>
 							Contact
 						</Link>
+						<Link
+							href="/game"
+							className="duration-200 text-zinc-400 hover:text-zinc-100"
+						>
+							Game
+						</Link>
 					</div>
 
 					<Link
