5dc8f7cce3cf0d0bc4e5470be04f0ecdd5ddb434
JuanBraco <juanbraco@JuanBracos-MacBook-Air.local>
2024-11-06 14:36:29 +0100

    new text

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-					My name is Juan Braco, I'm a software engineer currently studying at Ecole 42 and building stuff
+					My name is Jean-Baptiste de La Fage, I'm a Founder and CTO of Plume, a startup  backed by Y Combinator and Kima Ventures.
 				</h2>
 			</div>
 		</div>
